c536398f73a314c0cbb7fd642841df0acb9e1984
GunagiSa <GunagiSa@GSanjay.ap.uis.unisys.com>
2019-05-29 12:46:56 +0530

    Third commit.

--- FR-1/src/ModeChangeTest.java
+++ FR-1/src/ModeChangeTest.java
@@ -1,7 +1,7 @@
 //package com.sanjay.fr;
 
 /**
- * This program is to implement F&R tasks.
+ * This program is to implement F&R automation tasks.
  * @author Sanjay
  * @version 1.0
  * @
@@ -21,90 +21,81 @@ import java.util.Properties;
 
 public class ModeChangeTest {
 
-	public static void main(String[] args) throws Exception
-	{
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		
-		Properties properties = new Properties();
-		InputStream input = null;
-		input = new FileInputStream("E:\\Cmd\\Sanjay\\Test\\FR\\config\\config.properties");
-		properties.load(input);
-		
+
+//		Properties properties = new Properties();
+//		InputStream input = null;
+//		input = new FileInputStream("E:\\Cmd\\Sanjay\\Test\\FR\\config\\config.properties");
+//		properties.load(input);
+//		
 		String airline = args[0];
 		
-		String Url = properties.getProperty("urlTest");
-		String usernameUW = properties.getProperty("usernameUW");
-		String passwordUW = properties.getProperty("passwordUW");
-		String usernameMW = properties.getProperty("usernameMW");
-		String passwordMW = properties.getProperty("passwordMW");
+		/*
+		 * if( airline.equalsIgnoreCase("UW")) { UWClass.utilityUW(airline); }
+		 */
 		
-		String queryUW = "UPDATE cfd_schedule_change_mode SET schedule_change_mode = 1";
-		String checkTSM = "select * from schedule_state";
-		String checkSCRecords = "select* from T1UWRTG.genstatus";
-		
-//		String commitStmt = "commit";
-		
-		Class.forName("oracle.jdbc.driver.OracleDriver");  
-		if(airline.equalsIgnoreCase("UW"))
+		switch (airline) 
 		{
-			Connection conUW = DriverManager.getConnection(Url, usernameUW, passwordUW);
-			
+			case "UW":
+				UWClass.utilityUW(airline);
+			break;
 
-			System.out.println("Connection Established Successfully to UW DB and the DATABASE NAME IS:"+ conUW.getMetaData().getDatabaseProductName());
-			//step3 create the statement object  
-			Statement stmtUW = conUW.createStatement();  
-			
-			ResultSet rsTSM = stmtUW.executeQuery(checkTSM);
-			
-			boolean emptySetTSM = true;
-			while(rsTSM.next())
-			{
-				emptySetTSM = false;
-			}
-			
-			ResultSet rsSCRecords = stmtUW.executeQuery(checkSCRecords);
-			
-			boolean emptySetSCRecords = true;
-			while(rsTSM.next())
-			{
-				emptySetSCRecords = false;
-			}
-			
-			if(emptySetTSM == true && emptySetSCRecords ==true)
-			{
-			
-	//			Statement commitSt = conUW.createStatement();
-	//			String queryUW = "UPDATE cfd_schedule_change_mode SET schedule_change_mode = 1";//QueriesUtil.updateModeTest;
-				System.out.println("No TSM and SC records Exist.");
-				int count = stmtUW.executeUpdate(queryUW); 
-				
-	//			int checkCommit = stmtUW.executeUpdate(commitStmt);
-	
-				if (count > 0)
-				{
-					System.out.println("Schedule Change Mode is updated to manual.");
-				}
-				else
-				{
-					System.out.println("Not Updated.");
-				}
-			}
-			else
-			{
-				System.out.println("TSM and SC records exist.");
-			}
-			/*
-			 * if(checkCommit >0) { System.out.println("Commited"); } else {
-			 * System.out.println("Commit not done."); }
-			 */
-//			ResultSet rsUW = stmtUW.executeQuery(queryUW); 
-			
-//			while(rsUW.next())  
-//			System.out.println(rsUW.getString(1));  
-	
-			conUW.close();  
+			default:
+				System.out.println("No airline selected.");
+			break;
 		}
-		
+		/*
+		 * String Url = properties.getProperty("urlTest"); String usernameUW =
+		 * properties.getProperty("usernameUW"); String passwordUW =
+		 * properties.getProperty("passwordUW"); String usernameMW =
+		 * properties.getProperty("usernameMW"); String passwordMW =
+		 * properties.getProperty("passwordMW");
+		 * 
+		 * String queryUW =
+		 * "UPDATE cfd_schedule_change_mode SET schedule_change_mode = 1"; String
+		 * checkTSM = "select * from schedule_state"; String checkSCRecords =
+		 * "select* from T1UWRTG.genstatus";
+		 * 
+		 * // String commitStmt = "commit";
+		 * 
+		 * Class.forName("oracle.jdbc.driver.OracleDriver");
+		 * if(airline.equalsIgnoreCase("UW")) { Connection conUW =
+		 * DriverManager.getConnection(Url, usernameUW, passwordUW); System.out.
+		 * println("Connection Established Successfully to UW DB and the DATABASE NAME IS:"
+		 * + conUW.getMetaData().getDatabaseProductName()); Statement stmtUW =
+		 * conUW.createStatement(); ResultSet rsTSM = stmtUW.executeQuery(checkTSM);
+		 * boolean emptySetTSM = true;
+		 * 
+		 * while(rsTSM.next()) { emptySetTSM = false; }
+		 * 
+		 * ResultSet rsSCRecords = stmtUW.executeQuery(checkSCRecords); boolean
+		 * emptySetSCRecords = true;
+		 * 
+		 * while(rsSCRecords.next()) { emptySetSCRecords = false; }
+		 * 
+		 * if(emptySetTSM == true && emptySetSCRecords ==true) { // Statement commitSt =
+		 * conUW.createStatement(); // String queryUW =
+		 * "UPDATE cfd_schedule_change_mode SET schedule_change_mode = 1";//QueriesUtil.
+		 * updateModeTest; System.out.println("No TSM and SC records Exist."); int count
+		 * = stmtUW.executeUpdate(queryUW);
+		 * 
+		 * // int checkCommit = stmtUW.executeUpdate(commitStmt);
+		 * 
+		 * if (count > 0) {
+		 * System.out.println("Schedule Change Mode is updated to manual."); } else {
+		 * System.out.println("Not Updated."); } } else {
+		 * System.out.println("TSM and SC records exist."); }
+		 * 
+		 * if(checkCommit >0) { System.out.println("Commited"); } else {
+		 * System.out.println("Commit not done."); }
+		 * 
+		 * // ResultSet rsUW = stmtUW.executeQuery(queryUW); // while(rsUW.next()) //
+		 * System.out.println(rsUW.getString(1));
+		 * 
+		 * conUW.close(); }
+		 */
+
 	}
 
 }
